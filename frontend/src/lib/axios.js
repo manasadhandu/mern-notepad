5eb10a2b1b1d4d4c3f535139e2c46fc462d20097
manasadhandu <manasadhandu04@gmail.com>
2025-11-19 16:42:21 +0530

    render url axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL =
   import.meta.env.MODE === "development"
-    ? "http://localhost:5000/api"
+    ? "https://manuvarma-notes-builder.onrender.com//api"
     : "/api";
 
 const api = axios.create({
